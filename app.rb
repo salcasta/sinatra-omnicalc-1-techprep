863fe5ed4c30da82f026a82382db6ea955b30100
Salvador Castaneda <57605337+salcasta@users.noreply.github.com>
2023-08-02 01:41:48 +0000

    done with square, square root, and payment pages

--- app.rb
+++ app.rb
@@ -2,8 +2,48 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:new_square_calc)
+end
+
+get("/square/new") do
+  erb(:new_square_calc)
+end
+
+get("/square/results") do
+  @the_num = params.fetch("number").to_f
+  @the_result = @the_num ** 2 
+
+  erb(:square_results)
+end
+
+get("/square_root/new") do
+  erb(:new_square_root_calc)
+end
+
+
+get("/square_root/results") do
+  @the_num = params.fetch("number").to_f
+  @the_result = Math.sqrt(@the_num)
+
+  erb(:square_root_results)
+end
+
+get("/payment/new") do
+  erb(:new_payment_calc)
+end
+
+get("/payment/results") do
+@interest = ((params.fetch("apr").to_f / 100) / 12)
+@years = params.fetch("num_year").to_i * 12
+@amount = (params.fetch("num_amount").to_f)
+
+@numerator = @interest * @amount
+@denominator = 1 - ((1 + @interest) ** -@years)
+
+@payment = @numerator / @denominator
+
+@display_apr = (params.fetch("apr").to_f).round(5).to_fs(:percentage)
+@display_amount = (params.fetch("num_amount").to_f).to_fs(:currency)
+@display_payment = (@numerator / @denominator).to_fs(:currency)
+erb(:payment_results)
 end
